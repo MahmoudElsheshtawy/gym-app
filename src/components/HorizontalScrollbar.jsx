7c159f63ac06edb6bcad2ddfc28f6286a272d13e
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-09-23 23:55:53 +0300

    no

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -3,44 +3,57 @@ import { Box, Typography } from '@mui/material'
 import Bodypart from './Bodypart'
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+// import RightArrowIcon from '../assets/icons/right-arrow.png';
+// import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+// const LeftArrow = () => {
+// const { scrollPrev } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
+//   return (
+//     <Typography onClick={() => scrollPrev()} className="right-arrow">
+//       <img src={LeftArrowIcon} alt="right-arrow" />
+//      </Typography>
+//   );
+// };
 
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
+// const RightArrow = () => {
+//   const { scrollNext } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
+//   return (
+//     <Typography onClick={() => scrollNext()} className="left-arrow">
+//       <img src={RightArrowIcon} alt="right-arrow" />
+//     </Typography>
+//   );
+// };
 
 const HorizontalScrollbar = ({data,bodypart,setBodypart}) => {
 
   return (
-    <ScrollMenu  LeftArrow={LeftArrow} RightArrow={RightArrow}>
-     
-        {data.map((item)=>(
-            <Box key={item.id || item}
-             itemID={item.id || item}
-             title={item.id || item}
-             margin={"0 40px"}
+ 
+     <ScrollMenu>
+       {data.map((item)=>(
+            <Box 
+            key={item.id || item}
+            itemID={item.id || item}
+            title={item.id || item}
+            margin={"10px 40px  10px"}
              >
-              <Bodypart  item={item} bodypart={bodypart} setBodypart={setBodypart}/>
-            </Box>
+                
+              <Bodypart 
+               item={item}
+               bodypart={bodypart}
+               setBodypart={setBodypart}
+               />
+              
+            </Box> 
         ))}
-    </ScrollMenu>
+        
+        
+        </ScrollMenu>
+  
+     
+       
+  
   )
 }
 
--- src/components/bodypart.jsx
+++ src/components/bodypart.jsx
@@ -3,28 +3,31 @@ import Icon from'../assets/icons/gym.png'
 import { Stack, Typography } from '@mui/material'
 const Bodypart = ({item ,bodypart,setBodypart}) => {
   return (
-    <Stack
+
+     <Stack
     type="button"
     alignItems="center"
     justifyContent="center"
     className="bodypart-card"
     sx={{
-    borderTop:  bodypart === item ?   '4px solid #FF2625': '' ,
-    background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px'
-  
-  
-  
-  
-  }}
+    borderTop : bodypart === item ? '4px solid #FF2625': "",
+    background: '#aaa',
+    //  borderBottomLeftRadius: '20px',
+      width: '100px', height: '150px',
+       cursor: 'pointer', gap: '4px'
+}}
 
       >
 
    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
+    <Typography fontSize="20px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
 
 
 
     </Stack>
+
+   
+
   )
 }
 
